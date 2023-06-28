42443135c3796f991146f2f8a5710ca1c04eae94
ldeeley <lester.deeley@yahoo.com>
2023-06-28 16:10:39 +0100

    Adding a Eureka Discovery Server, Set up User-Service as a Eureka client to enable service discovery. Bake all into Docker containers

---
+++ eureka-service/src/main/java/org/example/EurekaServerApplication.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Hello world!
+ *
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApplication
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+}
--- user-service/src/main/java/com/example/rnralbumart/RnRUserServiceApplication.java
+++ user-service/src/main/java/com/example/rnralbumart/RnRUserServiceApplication.java
@@ -2,13 +2,14 @@ package com.example.rnralbumart;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-public class RnRAlbumArtApplication {
+@EnableDiscoveryClient
+public class RnRUserServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(RnRAlbumArtApplication.class, args);
+        SpringApplication.run(RnRUserServiceApplication.class, args);
     }
 
 }
